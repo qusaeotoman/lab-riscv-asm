b4c9c5b043595ae1028caa73675c2116be876b08
Qusae <qusae.otoman@gmail.com>
2025-11-25 01:58:28 +0300

    Add screenshots and update assembly file

--- algo.s
+++ algo.s
@@ -7,26 +7,25 @@ array:   .word 1, 2, 3, 4
 .text
 .globl main
 main:
-    la t0, array      # t0 = base address of array
-    li t1, 0          # t1 = i (index على القطر)
-    li t2, 0          # t2 = sum
+    la t0, array      
+    li t1, 0         
+    li t2, 0          
 
 loop:
-    li t3, 4          # N = 4
-    bge t1, t3, end   # if i >= 4 => end
+    li t3, 4         
+    bge t1, t3, end 
 
     li a0, 4
-    mul a1, t1, a0    # a1 = i*4
-    add a1, a1, t1    # a1 = i*4 + i
-    slli a1, a1, 2    # *4 (byte offset)
-    add a2, t0, a1    # address of array[i][i]
-    lw a3, 0(a2)      # a3 = array[i][i]
-    add t2, t2, a3    # sum += array[i][i]
+    mul a1, t1, a0    
+    add a1, a1, t1    
+    slli a1, a1, 2    
+    add a2, t0, a1    
+    lw a3, 0(a2)      
+    add t2, t2, a3    
 
     addi t1, t1, 1
     j loop
 
 end:
-    # لا توجد ecalls هنا
-    # النتيجة النهائية في t2
+  
     nop
